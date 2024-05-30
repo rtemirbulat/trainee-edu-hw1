a8938364930b037c9e09545858d8c4142019f5e3
rtemirbulat <equalityisnotequality@gmail.com>
2024-05-30 22:34:32 +0500

    renamed Main.java to HelloAndersen.java

--- src/main/java/com/rtemi/HelloAndersen.java
+++ src/main/java/com/rtemi/HelloAndersen.java
@@ -1,6 +1,6 @@
 package com.rtemi;
 
-public class Main {
+public class HelloAndersen {
     public static void main(String[] args) {
         System.out.println("Hello Andersen Lab!"); // should be replaced by a logging.Logger
     }

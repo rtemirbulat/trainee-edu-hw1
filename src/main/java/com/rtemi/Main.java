233cf0a3b2fec64edcded308f0bba28e95c42722
rtemirbulat <equalityisnotequality@gmail.com>
2024-05-30 22:21:45 +0500

    initial commit

---
+++ src/main/java/com/rtemi/Main.java
@@ -0,0 +1,7 @@
+package com.rtemi;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello Andersen Lab!"); // should be replaced by a logging.Logger
+    }
+}
